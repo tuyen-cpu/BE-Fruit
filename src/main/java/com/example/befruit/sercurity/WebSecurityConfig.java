0e1f139c143040dccd5d593071048e10ddf3cc37
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-10-31 09:21:49 +0700

    adjust WebSecurityConfig without WebSecurityConfigurerAdapter

--- src/main/java/com/example/befruit/sercurity/WebSecurityConfig.java
+++ src/main/java/com/example/befruit/sercurity/WebSecurityConfig.java
@@ -1,74 +1,53 @@
 package com.example.befruit.sercurity;
 
-import com.example.befruit.sercurity.service.UserDetail;
 import com.example.befruit.sercurity.service.UserDetailService;
-import com.example.befruit.service.impl.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.BeanIds;
-import org.springframework.security.config.annotation.SecurityBuilder;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
 import java.util.Arrays;
-import java.util.List;
+
 
 @Configuration
-@EnableWebSecurity
-
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+//@EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        // securedEnabled = true,
+        // jsr250Enabled = true,
+        prePostEnabled = true)
+public class WebSecurityConfig{
     @Autowired
     UserDetailService userDetailService;
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailService).passwordEncoder(passwordEncoder());
-    }
+
 @Bean
-@Override
-public AuthenticationManager authenticationManagerBean() throws Exception {
-    return super.authenticationManagerBean();
+public DaoAuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+    authProvider.setUserDetailsService(userDetailService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+
+    return authProvider;
 }
-//@Bean
-//public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//    CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        corsConfiguration.setAllowCredentials(true);
-//        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-//        corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-//                "Accept", "Authorization", "Origin, Accept", "X-Requested-With",
-//                "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-//        corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-//                "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-//        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-//
-//        http.authorizeRequests()
-//                .antMatchers("/**").permitAll()
-//                .and().csrf().disable().cors().configurationSource(request -> corsConfiguration);
-//
-//
-//        http.headers().frameOptions().disable();
-//
-//    return http.build();
-//}
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+
+@Bean
+public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception {
+    return authConfiguration.getAuthenticationManager();
+}
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
@@ -85,5 +64,8 @@ public AuthenticationManager authenticationManagerBean() throws Exception {
 
 
         http.headers().frameOptions().disable();
-    }
+
+    return http.build();
+}
+
 }
